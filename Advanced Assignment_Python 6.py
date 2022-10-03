6dd68f6e1975738d0022b3afaab53c22441b1f67
MedhaVashisth <112161904+MedhaVashisth@users.noreply.github.com>
2022-10-03 17:44:41 +0530

    Add files via upload

---
+++ Advanced Assignment_Python 6.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+1. Raised when the specified key is not found in the dictionary. Raised when an identifier is not found in the local or global namespace. Raised when trying to access a local variable in a function or method but no value has been assigned to it. Base class for all exceptions that occur outside the Python environment.
+
+2. An exception object is created when a Python script raises an exception. If the script explicitly doesn't handle the exception, the program will be forced to terminate abruptly.
+
+3. In Python, we catch exceptions and handle them using try and except code blocks. The try clause contains the code that can raise an exception, while the except clause contains the code lines that handle the exception.
+
+4. To avoid such a scenario, there are two methods to handle Python exceptions: Try – This method catches the exceptions raised by the program. Raise – Triggers an exception manually using custom exceptions.
+
+5. This clause is meant to define clean-up actions that must be performed regardless of whether an exception was raised or not. A finally clause must always be executed before leaving the try/except statement, whether an exception has occurred or not.
